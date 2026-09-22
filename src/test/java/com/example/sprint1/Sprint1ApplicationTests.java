c3bdc79d05dd4320137de748888fcb5d1ba0159b
ryogayamamoto0117-tech <311000617+ryogayamamoto0117-tech@users.noreply.github.com>
2026-09-22 16:49:47 +0900

    Add health endpoint test

--- src/test/java/com/example/sprint1/Sprint1ApplicationTests.java
+++ src/test/java/com/example/sprint1/Sprint1ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.sprint1;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(HelloController.class)
 class Sprint1ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void healthReturnOK() throws Exception{
+
+		mockMvc.perform(get("/health"))
+		       .andExpect(status().isOk())
+			   .andExpect(content().string("OK"));
 	}
 
 }
